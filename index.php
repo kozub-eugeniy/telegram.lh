4347075f2c4958e78e71bdb60cf6186ebdc9d10c
kozub-eugeniy <kozub.evgenij@gmail.com>
2017-03-19 12:49:27 +0200

    first push

--- index.php
+++ index.php
@@ -5,3 +5,8 @@
  * Date: 19.03.2017
  * Time: 12:09
  */
+
+$token = '332153319:AAE5mbWS3cnriBvwCh9XqnkzF1UhFaaig90';
+
+$output = file_get_contents('php://input');
+file_put_contents('logs.txt', $output);
